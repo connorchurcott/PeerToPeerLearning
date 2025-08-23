1859a0d080b88a95843a758d64a8ecac4db0e6f5
connorchurcott <connorchurcott@gmail.com>
2025-08-23 16:32:18 -0700

    initial

---
+++ src/data/ClassesData.js
@@ -0,0 +1,54 @@
+
+
+// List of all classes we offer and all information displayed on pages
+
+export const ClassesData = [
+    {
+        slug: "ap-calculus",
+        title: "AP Calculus",
+        image: "/images/apcalculus.jpg",
+        instructorName: "Godfrey, First Elden Lord",
+        instructorImg: "/images/instructors/godfrey.jpg",
+        classDuration: "September 02 - December 02, 2025",
+        dayTime: "[Sept 3 - Dec 15]: Every Tuesday at 5:00pm",
+        enrollmentCost: "$5",
+        shortDescription: "Covers basic calculus as taught in AP calculus",
+        longDescription: "This class is great for students who are currently in AP Calculus, and want supplemental lessons, or are planning to take AP Calculus and want an early start on the material. Our courses covers all major topics of AP Calculus to ensure students are ready to tackle any type of question that will be asked on AP Exams.",
+        bulletList: [
+            "Limits",
+            "Derivatives",
+            "Integrals",
+            "Applications",
+            "Series"
+        ],
+    },
+    {
+        slug: "ap-history",
+        title: "AP History",
+        image: "/images/aphistory.jpeg",
+        shortDescription: "Covers history including WW1 and WW2",
+        longDescription: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+    },
+    {
+        slug: "ap-english", 
+        title: "AP English",
+        image: "/images/apenglish.jpg",
+        shortDescription: "We be reading books n shi",
+        longDescription: "ccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+    },
+    {
+        slug: "ap-english", 
+        title: "AP English",
+        image: "/images/apenglish.jpg",
+        shortDescription: "We be reading books n shi",
+        longDescription: "ccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+    }
+    ,
+    {
+        slug: "ap-english", 
+        title: "AP English",
+        image: "/images/apenglish.jpg",
+        shortDescription: "We be reading books n shi",
+        longDescription: "ccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+    }
+];
